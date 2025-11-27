99ae88ab624f0608dba1b1aeecbba67a659b0cba
Lettimaks <155953018+It-SB@users.noreply.github.com>
2025-11-27 16:21:15 +0200

    Remove .history from repo

--- .history/src/models/test/TestModel_20251112150342.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const TestSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.Test || mongoose.model("Test", TestSchema);
